7c6da2dfa637b238da7424023db09df362b3f650
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 18:51:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
 
 holiday_supplies.each_line do |line|
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
+    puts "#{season}:"
     holiday.each do |holiday, supplies|
     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
   end
