ca4c0a2af7dbcc144c24a4df68ba9d1177476d5d
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 18:26:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,7 +36,9 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_supplies[:spring].each do |holiday_hash, supply|
 
+  supply << "Grill"
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
