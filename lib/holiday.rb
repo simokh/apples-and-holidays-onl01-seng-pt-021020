c7cd660f89690924c49f407c8def5151f2cb6cae
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 18:17:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_supplies[:winter] && [:new_years] => "Balloons"
 
-
+binding.pry 
 end
 
 
