a576f3ee635dba048a618ebc0544e76ddba92cf7
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 18:48:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,12 +66,20 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
 holiday_supplies.each_line do |line|
-  puts line.chomp
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
   end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
-end
+  holiday_hash.map do |season, holiday|
+      holiday.map do |holiday, supply|
+        holiday if supply.include?("BBQ")
+      end
+    end.flatten.compact
+  end
