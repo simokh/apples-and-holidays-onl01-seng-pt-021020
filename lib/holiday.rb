8e477988dbe35efd03ce907c6496a5843ce546f9
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 18:45:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-return holiday_supplies
+puts  holiday_supplies
 end
 
 def all_holidays_with_bbq(holiday_hash)
