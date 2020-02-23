be60df85bd17b74c3288a7dc6ded36ffba7229f1
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 18:12:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,7 +20,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
   # holiday_supplies[:summer][:fourth_of_july].shift [2]
   holiday_supplies[:summer][:fourth_of_july][1]
-  holiday_supplies[:summer][:fourth_of_july]
     # binding.pry
 end
 
